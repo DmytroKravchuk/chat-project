92684b769b418efa6615b9ca6ab060ab028c9c24
Dmytro Kravchuk <Dmytro.Kravchuk@infopulse.com>
2019-07-08 22:59:50 +0300

    refactor ListMessage, Chat

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,14 +3,13 @@ import css from './chat.module.scss';
 import AddMessageContainer from '../message/addMessage';
 import ListMessageContainer from '../message/listMessage';
 
-const Chat = () => {
-	return (
-		<div className={css.chat_wrapper}>
-			<h2 className={css.chat_header}>CHAT</h2>
-			<ListMessageContainer />
-			<AddMessageContainer />
-		</div>
-	)
-};
+const Chat = () => (
+	<div className={css.chat_wrapper}>
+		<h2 className={css.chat_header}>CHAT</h2>
+		<ListMessageContainer />
+		<AddMessageContainer />
+	</div>
+);
+
 
 export default Chat;
--- src/components/message/listMessage/ListMessage.jsx
+++ src/components/message/listMessage/ListMessage.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import css from './listMessage.module.scss';
 import Message from '../Message';
 
-export const ListMessage = (props) => {
-	return (
-		<div className={css.chat_message_box}>
-			{
-				props.messages && props.messages.length !== 0
-				? props.messages.map((data, index) => {
-					return <Message data={data} key={index} />
-				})
-				: <div className={css.chat_no_message}>No messages</div>
-			}
-		</div>
-	)
-};
+export const ListMessage = (props) => (
+	<div className={css.chat_message_box}>
+		{
+			props.messages && props.messages.length !== 0
+			? props.messages.map((data, index) => {
+				return <Message data={data} key={index} />
+			})
+			: <div className={css.chat_no_message}>No messages</div>
+		}
+	</div>
+);
