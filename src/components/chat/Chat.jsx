f77ac6ac461a385cf33120fafc008c57943966fe
Dmytro Kravchuk <Dmytro.Kravchuk@infopulse.com>
2019-07-08 21:49:10 +0300

    refactor Chat.jsx

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,14 +3,14 @@ import css from './chat.module.scss';
 import AddMessageContainer from '../message/addMessage';
 import ListMessageContainer from '../message/listMessage';
 
-export default class Chat extends React.Component {
-	render() {
-		return (
-			<div className={css.chat_wrapper}>
-				<h2 className={css.chat_header}>CHAT</h2>
-				<ListMessageContainer />
-				<AddMessageContainer />
-			</div>
-		)
-	}
-}
+const Chat = () => {
+	return (
+		<div className={css.chat_wrapper}>
+			<h2 className={css.chat_header}>CHAT</h2>
+			<ListMessageContainer />
+			<AddMessageContainer />
+		</div>
+	)
+};
+
+export default Chat;
