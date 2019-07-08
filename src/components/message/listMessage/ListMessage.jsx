071ce9b8dd1f58fca2cd8e809e352fceaa5d3c47
Dmytro Kravchuk <Dmytro.Kravchuk@infopulse.com>
2019-07-08 23:38:46 +0300

    updated styles listMessage

--- src/components/message/listMessage/ListMessage.jsx
+++ src/components/message/listMessage/ListMessage.jsx
@@ -5,12 +5,14 @@ import uniqid from 'uniqid';
 
 export const ListMessage = (props) => (
 	<div className={css.chat_message_box}>
-		{
-			props.messages && props.messages.length !== 0
-			? props.messages.map((data) => {
-				return <Message data={data} key={uniqid()} />
-			})
-			: <div className={css.chat_no_message}>No messages</div>
-		}
+		<div className={css.chat_scroll_container}>
+			{
+				props.messages && props.messages.length !== 0
+				? props.messages.map((data) => {
+					return <Message data={data} key={uniqid()} />
+				})
+				: <div className={css.chat_no_message}>No messages</div>
+			}
+		</div>
 	</div>
 );
